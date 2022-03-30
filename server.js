2b227386a703bf81d9a78a0d2cc7f19c06eaf972
rizalmab <rizalmahadiab@gmail.com>
2022-03-31 00:39:25 +0800

    Changed root package.json

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const publicPath = path.join(__dirname, "..", "public");
+console.log("publicPath: ", publicPath);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
